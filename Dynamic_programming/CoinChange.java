7bfff678006e76d7e9d13cf928894c2670a55f06
2003devansh <faujdardevansh@gmail.com>
2025-05-22 21:19:48 +0530

    Refactor CoinChange class to improve readability and add DP table output

--- Dynamic_programming/CoinChange.java
+++ Dynamic_programming/CoinChange.java
@@ -1,28 +1,43 @@
 package Dynamic_programming;
 
-// to find the maximum number of ways to make a sum using given coins
-
 public class CoinChange {
 
     public static void main(String[] args) {
-        
+        int[] coins = {1, 2, 3};
+        int amount = 4;
+        CoinChange cc = new CoinChange();
+        int ways = cc.coinChange(coins, amount);
+        System.out.println("Number of ways to make sum " + amount + ": " + ways);
     }
 
     public int coinChange(int[] coins , int amount){
         int n  = coins.length ; 
         int[][] dp = new int[n+1][amount+1];
 
+        // Base case: There's 1 way to make amount 0 with any number of coins (by picking none)
+        for(int i = 0; i <= n ; i++){
+            dp[i][0] = 1; 
+        }
 
-        for(int i = 1 ; i <= n ;i++ ){
+        for(int i = 1 ; i <= n ;i++){
             for(int j = 1 ; j <= amount ; j++){
-                if(coins[i-1] <= j){
-                    dp[i][j] = Math.max(dp[i][j - coins[i-1]] + 1 , dp[i-1][j]);
+                if(coins[i-1] <= j ){
+                    dp[i][j] = dp[i][j - coins[i-1]] + dp[i-1][j];
                 }else{
-                    dp[i][j] = dp[i-1][j];
+                    dp[i][j] = dp[i-1][j]; 
                 }
             }
         }
-        return dp[n][amount];
+
+        // Print DP table
+        System.out.println("DP Table:");
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= amount; j++) {
+                System.out.print(dp[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        return dp[n][amount]; 
     }
-    
 }
